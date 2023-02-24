395e5c11bfab37dc89a089d01aa297b9190235ed
Csangi <cs19N@my.fsu.edu>
2023-02-24 13:43:36 -0500

    Added new UI for opening screen

---
+++ Assets/Scripts/OpeningScripts/OpeningScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpeningScript : MonoBehaviour
+{
+    public Canvas openingCanvas, levelCanvas, shopCanvas;
+
+    public void openLevel()
+    {
+        openingCanvas.enabled = false;
+        levelCanvas.enabled = true;
+    }
+    public void returnLevel()
+    {
+        levelCanvas.enabled = false;
+        openingCanvas.enabled = true;
+    }
+
+    public void openShop()
+    {
+        openingCanvas.enabled = false;
+        shopCanvas.enabled = true;
+    }
+    public void returnShop()
+    {
+        shopCanvas.enabled = false;
+        openingCanvas.enabled = true;
+    }
+
+    public void playLevelOne()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+    public void playLevelTwo()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
