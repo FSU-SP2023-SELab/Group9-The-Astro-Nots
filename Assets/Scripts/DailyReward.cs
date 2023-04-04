11afdcd692d0ae49ab8ddc4a9a3f0a0feee83d85
eae19i <112736949+eae19i@users.noreply.github.com>
2023-04-03 22:09:28 -0400

    Finalizes Daily Rewards, Implements Daily Rewards GUI, Bug Fixes

--- Assets/Scripts/DailyReward.cs
+++ Assets/Scripts/DailyReward.cs
@@ -2,43 +2,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
-public struct PlayerState                                                        //Placeholder subject to removal
-{
-    public int coins;
-}
+public class DailyReward : MonoBehaviour {
 
-public class DailyReward : MonoBehaviour
-{
+    public GameObject rewardPanel;
+    public Text rewardText;
+    public Text loginStreakText;
+
     private int reward = 100;
 
+    public string coinBalanceKey = "Coins";
     public string lastLoginKey = "LastLogin";
     public string loginStreakKey = "LoginStreak";
-    
-    public PlayerState playerState;                                              //Placeholder struct to demonstrate how PlayerState would be updated moving forward
 
+    public void closeRewardOverlay()
+    {
+        rewardPanel.SetActive(false);
+        Debug.Log("Disable Overlay Called");
+    }
+
+    //Initalize Player Prefs
+    void Awake()
+    {
+        if (!PlayerPrefs.HasKey(coinBalanceKey))
+        {
+            PlayerPrefs.SetInt(coinBalanceKey, 100); //Save 100 starting coins to player's balance
+        }
+        if (!PlayerPrefs.HasKey(lastLoginKey)) {
+            PlayerPrefs.SetString(lastLoginKey, DateTime.Now.ToString()); //Initialize lastLogin as a string under the player's lastLoginKey
+        }
+        if(!PlayerPrefs.HasKey(loginStreakKey)) {
+            PlayerPrefs.SetInt(loginStreakKey, 0); //Initialize login streak to 0
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
+        int coins = PlayerPrefs.GetInt("Coins");
         DateTime lastLogin = DateTime.MinValue;
         int loginStreak = 0;
 
         if (PlayerPrefs.HasKey(lastLoginKey))
         {
-            lastLogin = DateTime.Parse(PlayerPrefs.GetString(lastLoginKey));     //Use DateTime parse method to translate string into DateTime object
+            lastLogin = DateTime.Parse(PlayerPrefs.GetString(lastLoginKey)); //Use DateTime parse method to translate string into DateTime object
         }
 
-        if (PlayerPrefs.HasKey(loginStreakKey)) {                                 //Retrieve login streak and calculate how large player's reward should be
+        if (PlayerPrefs.HasKey(loginStreakKey))
+        { //Retrieve login streak and calculate how large player's reward should be
             loginStreak = PlayerPrefs.GetInt(loginStreakKey);
 
-            if (loginStreak > 0 && loginStreak % 5 == 0 && reward < 500)      
-            {
-                reward = 100 * (loginStreak / 5);
-
+            if (reward < 500) {
+                reward = Mathf.Min(100 + 50 * (loginStreak - 1), 500);
                 Debug.Log("Reward increased to " + reward);
             }
+
         }
 
         TimeSpan timeSinceLastLogin = DateTime.Now - lastLogin;
@@ -46,16 +67,23 @@ public class DailyReward : MonoBehaviour
         Debug.Log("Player's Last Login: " + lastLogin + "\n");
         Debug.Log("Time Since Last Login: " + timeSinceLastLogin + "\n");
 
-        if (timeSinceLastLogin.TotalHours >= 24)                                 //If time since last login >24 && <48 hours give player reward, otherwise reset streak
-        {
-            if (timeSinceLastLogin.TotalHours < 48) {  
-                loginStreak += 1;
-                playerState.coins += reward;
+        Debug.Log("Player Coin Balance: " + coins); //Debugging output for player coin balance
 
-                Debug.Log("Login Streak: " + loginStreak + " Days\n");
-            } else {
+        if (timeSinceLastLogin.TotalHours >= 24  && timeSinceLastLogin.TotalHours < 48) //If time since last login >24 && <48 hours give player reward, otherwise reset streak
+        {
+                loginStreak += 1;
+                coins += reward;
+
+                rewardText.text = reward.ToString(); // Show the player what their reward is for the day
+                loginStreakText.text = "Current Streak: " + loginStreak.ToString() + " Days"; // Show the player what their login streak is that day
+
+                rewardPanel.SetActive(true); // Show the reward panel
+        }
+        else
+        {
                 Debug.Log("Last login time >48 hours ago, login streak reset to 0");
                 loginStreak = 0;
+<<<<<<< Updated upstream
             }
             
             PlayerPrefs.SetString(lastLoginKey, DateTime.Now.ToString());        //Save lastLogin as a string under the player's lastLoginKey
@@ -63,7 +91,19 @@ public class DailyReward : MonoBehaviour
             Debug.Log("New last login time saved to play prefs: " + DateTime.Now.ToString() + "\n");
 
             PlayerPrefs.Save();
+=======
+                rewardPanel.SetActive(false); // Hide the reward panel
+>>>>>>> Stashed changes
         }
+
+            PlayerPrefs.SetString(lastLoginKey, DateTime.Now.ToString()); //Save lastLogin as a string under the player's lastLoginKey
+            PlayerPrefs.SetInt(loginStreakKey, loginStreak); //Save current long streak under player's loginStreakKey
+
+            PlayerPrefs.SetInt("Coins", coins);
+
+            Debug.Log("New last login time saved to play prefs: " + DateTime.Now.ToString() + "\n");
+            Debug.Log("New coin balanced saved to player prefs: " + coins.ToString() + "\n");
+        PlayerPrefs.Save();
     }
 
     // Update is called once per frame
@@ -71,4 +111,9 @@ public class DailyReward : MonoBehaviour
     {
         //link to UI?
     }
+<<<<<<< Updated upstream
 }
+=======
+
+}
+>>>>>>> Stashed changes
---
+++ Assets/Scripts/FlushPlayerPrefs.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//WARNING: THIS SCRIPT DELETES ALL PLAYER PREFS FOR TESTING PURPOSES
+public class FlushPlayerPrefs : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
