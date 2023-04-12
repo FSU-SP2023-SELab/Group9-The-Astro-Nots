944df5fc0d399353145c5c7b4699ee7e6f7cf140
Csangi <cs19N@my.fsu.edu>
2023-04-11 23:12:14 -0400

    New Daily Rewards GUI fix merge

---
+++ Assets/Scripts/PlayerBalanceDisplay.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerBalanceDisplay : MonoBehaviour
+{
+    public Text coinText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int coins = PlayerPrefs.GetInt("Coins");
+        coinText.text = "$" + coins.ToString();
+    }
+
+    // Update is called once per frame
+    void Update() { }
+}
